a3c633d3fd0bfb5438527081ef45bffc2850d39f
HardiikGit <hardiik003@gmail.com>
2025-12-11 14:33:36 +0530

    sdfsd

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -1,5 +1,34 @@
+import React from 'react';
 import { FaRightLong } from 'react-icons/fa6';
 import { IoStarSharp } from 'react-icons/io5';
+const bannerData = {
+ heading: "WE'LL FIND FOR YOU THE \n BEST CAR FOR LEASING",
+ review: {
+  rating: 5,
+  label: "Sales-Quality"
+ },
+ ctaButton: {
+  text: "Check Your Car",
+  url: "#CheckYourCar"
+ },
+ steps: [
+  {
+   id: 1,
+   text: "1. Choose From 1200 New Cars",
+   className: "O1"
+  },
+  {
+   id: 2,
+   text: "2. Choose Equipment",
+   className: "O2"
+  },
+  {
+   id: 3,
+   text: "3. Contact Us",
+   className: "O3"
+  }
+ ]
+};
 
 function BannerSection() {
  return (
@@ -8,48 +37,48 @@ function BannerSection() {
     <div className="row">
      <div className="col-lg-12">
       <div className="Banner-Contant d-flex justify-space-between">
+
        <div className="Banner-Heading">
-        <h1>WE'LL FIND FOR YOU THE <br /> BEST CAR FOR LEASING</h1>
+        <h1 style={{ whiteSpace: 'pre-line' }}>
+         {bannerData.heading}
+        </h1>
        </div>
+
        <div className="Banner-Review">
         <div className="Review-Content d-flex align-items-center flexcolumn">
          <div className="Banner-Stars">
           <ul className="d-flex align-items-center">
-           <li className="Stars"><IoStarSharp /></li>
-           <li className="Stars"><IoStarSharp /></li>
-           <li className="Stars"><IoStarSharp /></li>
-           <li className="Stars"><IoStarSharp /></li>
-           <li className="Stars"><IoStarSharp /></li>
+           {[...Array(bannerData.review.rating)].map((_, index) => (
+            <li key={index} className="Stars">
+             <IoStarSharp />
+            </li>
+           ))}
           </ul>
          </div>
          <div className="Mid-Line"></div>
          <div className="Sales-Quality">
-          <p>Sales-Quality</p>
+          <p>{bannerData.review.label}</p>
          </div>
         </div>
        </div>
       </div>
       <div className="Main-Button">
-       <a href='#CheckYourCar' className="align-items-center">
-        Check Your Car <FaRightLong />
+       <a href={bannerData.ctaButton.url} className="align-items-center">
+        {bannerData.ctaButton.text} <FaRightLong />
        </a>
       </div>
-      <div className="Purchase-Steps d-flex  flexwrap">
-       <div className="O1">
-        <p>1. Choose From 1200 New Cars</p>
-       </div>
-       <div className="O2">
-        <p>2. Choose Equipment</p>
-       </div>
-       <div className="O3">
-        <p>3. Contact Us</p>
-       </div>
+      <div className="Purchase-Steps d-flex flexwrap">
+       {bannerData.steps.map((step) => (
+        <div key={step.id} className={step.className}>
+         <p>{step.text}</p>
+        </div>
+       ))}
       </div>
      </div>
     </div>
    </div>
   </div>
- )
+ );
 }
 
 export default BannerSection;
--- src/components/VIdeoSec.jsx
+++ src/components/VIdeoSec.jsx
@@ -1,19 +1,21 @@
 function VideoSec() {
  return (
-  <video src="/vid.mp4" autoPlay loop>
-   <div className="sdfsd">
+  // <video src="/vid.mp4" autoPlay loop muted className="relative">
+   <div className="VideoSection">
     <div className="container">
      <div className="row">
       <div className="col-lg-12">
-       <div className="para">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corporis minima nisi.</p>
+       <div className="VideoContent">
+        <div className="para">
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos corporis minima nisi.</p>
+        </div>
        </div>
       </div>
      </div>
     </div>
    </div>
-  </video>
+  // </video>
  )
 }
 
-export default VideoSec
+export default VideoSec;
